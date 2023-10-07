25f3465dc620a17b651dc82f302355d6ea590f99
Hallab <ibrahimhabeebolawale@gmail.com>
2023-10-07 03:01:30 +0100

    first commit

---
+++ src/components/LandingSection.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+import Hallab from './Hallab.jpg'
+
+const greeting = "Hello, I am Hallab!";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React and more";
+
+// Implement the UI for the LandingSection component according to the instructions.
+// Use a combination of Avatar, Heading and VStack components.
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+    <VStack spacing={20}>
+      <Avatar src={Hallab}
+      size='2xl'
+      name="Hallab" 
+      />
+      <Heading as='h3' size='md' noOfLines={1}>{greeting}</Heading>
+      <VStack spacing={6}>
+        <Heading as='h1' size='3xl' noOfLines={1}>{bio1}</Heading>
+        <Heading as='h1' size='3xl' noOfLines={1}>{bio2}</Heading>
+      </VStack>
+    </VStack>
+
+  </FullScreenSection>
+);
+
+export default LandingSection;
